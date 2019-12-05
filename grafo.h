ac496f48b4b09eb5497ce9251d7b0678f9cfa0d4
Gonzalo <gonzalo.alfacaso@gmail.com>
2019-12-05 16:25:52 -0500

    feat(Baconhead78): vtk reader for voronoi

--- grafo.h
+++ grafo.h
@@ -109,7 +109,101 @@ Grafo<pair<float,float>,V>::Grafo(int opcion){
 	switch (opcion)
 	{
 	case 1:{//vtk
-		/*code*/
+	ifstream myfile;
+	string file_path;
+	std::cout << "\nIngrese el nombre de archivo: \n";
+	std::cin >> file_path;
+	myfile.open(file_path);
+	string word;
+	Grafo<pair<float,float>,false>* graph = new Grafo<pair<float,float>,false>();
+
+	while(myfile >> word){
+		if (word == "POINTS"){
+			myfile >> word;
+			int node_q  = stoi(word);
+
+			myfile >> word;
+			myfile >> word;
+
+			for (int i=0; i < node_q; ++i){
+				int x = stoi(word);
+				myfile >> word;
+				int y = stoi(word);
+
+				graph->addNode({x,y});
+
+				myfile >> word;
+				myfile >> word;
+			}
+		}
+		if (word == "EDGE"){
+			myfile >> word;
+			int node_q  = stoi(word);
+
+			myfile >> word;
+			myfile >> word;
+
+			for (int i=0; i < node_q; ++i){
+				int x = stoi(word);
+				myfile >> word;
+				int y = stoi(word);
+				myfile >> word;
+				int p = stoi(word);
+				myfile >> word;
+				int q = stoi(word);
+				myfile >> word;
+				int w = stoi(word);
+
+				graph->addEdge({x,y}, {p,q},w);
+
+				myfile >> word;
+				myfile >> word;
+			}
+		}
+
+		if (word == "CELL"){
+			myfile >> word;
+			int node_q = stoi(word);
+
+			myfile >> word;
+			myfile >> word;
+
+			for (int i=0; i < node_q; ++i){
+				int q = stoi(word);
+				int temp;
+				myfile >> word;
+				
+				temp = stoi(word);
+				std::vector<int> node_x;
+				std::vector<int> node_y;
+
+				for (int j=0; j < q-1; ++j){
+					node_x.push_back(stoi(word));
+					myfile >> word;
+					node_y.push_back(stoi(word));
+				}
+				node_x.push_back(stoi(word));
+				node_y.push_back(temp);
+
+				for (int j=0; j < q; ++i){
+					int x = node_x[i];
+					int y = node_y[i];
+					int p,q;
+					if (q == q-1){
+						p = node_x[i+1];
+						q = node_y[i+1];
+					} else {
+						p = node_x[0];
+						q = node_y[0];
+					}
+					graph->addEdge({x,y}, {p,q});
+				}
+
+			}
+
+		}
+	}
+
 		break;
 	}
 
--- vtk_reader.h
+++ vtk_reader.h
@@ -52,5 +52,47 @@ void create_graph(string file_path){
 				myfile >> word;
 			}
 		}
+
+		if (word == "CELL"){
+			myfile >> word;
+			int node_q = stoi(word);
+
+			myfile >> word;
+			myfile >> word;
+
+			for (int i=0; i < node_q; ++i){
+				int q = stoi(word);
+				int temp;
+				myfile >> word;
+				
+				temp = stoi(word);
+				std::vector<int> node_x;
+				std::vector<int> node_y;
+
+				for (int j=0; j < q-1; ++j){
+					node_x.push_back(stoi(word));
+					myfile >> word;
+					node_y.push_back(stoi(word));
+				}
+				node_x.push_back(stoi(word));
+				node_y.push_back(temp);
+
+				for (int j=0; j < q; ++i){
+					int x = node_x[i];
+					int y = node_y[i];
+					int p,q;
+					if (q == q-1){
+						p = node_x[i+1];
+						q = node_y[i+1];
+					} else {
+						p = node_x[0];
+						q = node_y[0];
+					}
+					graph->addEdge({x,y}, {p,q});
+				}
+
+			}
+
+		}
 	}
 };
